220324f1cd8385589a2f9aa198c7e06f6108f260
Arthur Carneiro <123193887+arthurcarneiro1@users.noreply.github.com>
2025-11-10 21:26:04 -0300

    Add functionality to repeat input text

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,9 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+texto = input("Digite um texto: ")
+quantidade = int(input("Digite um número inteiro: "))
+
+resultado = texto * quantidade
+
+print("Texto repetido:")
+print(resultado)
